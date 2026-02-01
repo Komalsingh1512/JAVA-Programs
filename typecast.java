f2680090d9e75e5828523d7f1ff517191d7d35b2
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-02-01 18:06:55 +0530

    third commit

---
+++ typecast.java
@@ -0,0 +1,9 @@
+public class typecast {
+    public static void main (String[] args){
+        System.out.println("Typecasting is performed:");
+        int myInt = 9;
+        double myDouble = myInt; // Automatic casting: int to double
+        System.out.println(myInt);   
+        System.out.println(myDouble); 
+    }
+}
