f464c140f6a9f3c1608c5b26aba5697fc9099f63
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-02-01 17:57:58 +0530

    second commit

--- first.java
+++ first.java
@@ -1,5 +1,15 @@
 public class first {
     public static void main (String[] args){
         System.out.println("Hi my name is KOMAL KUMARI SINGH ");
-    }
+    int myNum = 5;               // integer (whole number)
+    float myFloatNum = 5.99f;    // floating point number
+    char myLetter = 'D';         // character
+    boolean myBool = true;       // boolean
+    String myText = "Hello";     // String    
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
 }
