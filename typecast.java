d73b63a1a2f2bdc1340d7379469fa3bd24a9ab66
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-02-01 18:26:59 +0530

    first commit

--- typecast.java
+++ typecast.java
@@ -4,9 +4,11 @@ public class typecast {
         int myInt = 9;
         int myInt2 = 10;
         String name = String.valueOf(myInt2);
+        String name1 = String.valueOf(myInt);
         double myDouble = myInt; // Automatic casting: int to double
         System.out.println(myInt);   
         System.out.println(myDouble); 
         System.out.println(name);
+        System.out.println(name1);
     }
 }
